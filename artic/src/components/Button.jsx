58b25aeb1daee1e4657b67d5778c36f4672dd80e
Aman <amansahu5051@gmail.com>
2024-02-23 12:44:29 +0530

    css: hover effect on button

--- artic/src/components/Button.jsx
+++ artic/src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({
     ...props
     
 }) => {
-  return <button type={type} className={`rounded-xl px-3 py-2 active:scale-95 text-sm bg-cyan-600 ${bgColor} ${textColor} ${className}`} {...props}>
+  return <button type={type} className={`rounded-xl px-3 py-2 hover:scale-105 active:scale-95 text-sm bg-cyan-600 hover:bg-cyan-800 ${bgColor} ${textColor} ${className}`} {...props}>
       {children}
     </button>
   
