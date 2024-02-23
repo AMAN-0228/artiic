e3a55c44c2af7c37ad123c095155828d0e99450f
Aman <amansahu5051@gmail.com>
2024-02-23 11:21:24 +0530

    css: post

--- artic/src/pages/Post.jsx
+++ artic/src/pages/Post.jsx
@@ -1,14 +1,12 @@
 import  { useEffect, useState } from 'react'
-import { Button, Container, SinglePost } from '../components';
+import {  Container, SinglePost } from '../components';
 import { useNavigate, useParams } from 'react-router-dom';
 import appwriteService from '../appwrite/config';
 import { useSelector } from 'react-redux';
 
 const Post = () => {
     const { slug } = useParams();
-    const navigate = useNavigate()
     const [post, setPost] = useState('');
-    const userData = useSelector(state => state.auth.userData)
     const loadPost = async() => {
       try {
         const data = await appwriteService.getPostBySlug(slug)
@@ -23,22 +21,9 @@ const Post = () => {
         loadPost()
     },[slug])
   return (
-    <div className='w-full px-3'>
-      <Container className='flex '>
-        {/* left section (post) */}
-        <div className='w-3/4'>
-      <SinglePost post={post}  />          
-        </div>
-
-      {/* right section (list of posts) */}
-      <div className='pl-5 w-1/4 '>
-        {
-          userData && userData?.$id === post?.userId &&
-        <div className='text-center flex  flex-wrap gap-2 md:ml-10 w-full  h-14'>{}
-        </div>
-        }
-      </div>
-
+    <div className='w-full '>
+      <Container >
+      <SinglePost post={post}  />
       </Container>
     </div>
   )
